7231a159aa1254fdcc2ec777fbb049cc7d703955
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-07-15 15:20:57 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,7 @@
+list1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+list2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+vi=str(input())
+for s in range(20):
+  if(vi==list1[s]):
+    print(list2[s])
+    break
